daf09b0b85b06243a4173e0483ae667716ea2486
Elizabeth Kennedy <eakennedy.2412@gmail.com>
2022-09-09 17:28:07 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-// TODO: Enable strict mode
+"use strict";
+//Exercise 1: Enable Strict Mode
 
-// TODO: Fix the following parameter list
-function parseToJSON(data, data) {
-  // TODO: Add a try/catch block to
-  // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
+console.log("Exercise 2: Using Try/Catch\n==========\n");
+console.log("Exercise 3: Function Syntax Debugging\n==========\n");
+//Fix the following parameter list
+function parseToJSON(data) {
+  // TODO: Add a try/catch block to attempt to convert 'data' to JSON
+  try {
+    console.log(data)
+    return JSON.parse(data); 
+  } catch (err) {
+    // TODO: if an exception is raised, print the error to the console and return null
+    console.log(`error: ${err}`)
+    alert("Cannot parse data to JSON");
+    return null;
+  }  
 }
-
-let failData = "<h1>Hello World!</h1>"
+let failData = ("Hello JS Error Handling!")
 
 let passData = JSON.stringify({ success: true });
 
